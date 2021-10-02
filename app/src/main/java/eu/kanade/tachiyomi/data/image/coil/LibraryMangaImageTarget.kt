ec6cff8ad9b561c271c59a0cec12408dd60488d2
Jays2Kings <jays@outlook.com>
2021-10-01 22:50:33 -0400

    more improvements to bestColor for covers

--- app/src/main/java/eu/kanade/tachiyomi/data/image/coil/LibraryMangaImageTarget.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/image/coil/LibraryMangaImageTarget.kt
@@ -71,12 +71,15 @@ fun Palette.getBestColor(): Int? {
     val vibPopulation = vibrantSwatch?.population ?: -1
     val domLum = dominantSwatch?.hsl?.get(2) ?: -1f
     val mutedPopulation = mutedSwatch?.population ?: -1
+    val mutedSaturationLimit = if (mutedPopulation > vibPopulation * 3f) 0.1f else 0.25f
     return when {
         (dominantSwatch?.hsl?.get(1) ?: 0f) >= .25f &&
-            domLum < .8f && domLum > .2f -> dominantSwatch?.rgb
+            domLum <= .8f && domLum > .2f -> dominantSwatch?.rgb
         vibPopulation >= mutedPopulation * 0.75f -> vibrantSwatch?.rgb
         mutedPopulation > vibPopulation * 1.5f &&
-            (mutedSwatch?.hsl?.get(1) ?: 0f) > if (mutedPopulation > vibPopulation * 3f) 0.1f else 0.25f -> mutedSwatch?.rgb
-        else -> vibrantSwatch?.rgb
+            (mutedSwatch?.hsl?.get(1) ?: 0f) > mutedSaturationLimit -> mutedSwatch?.rgb
+        else -> arrayListOf(vibrantSwatch, lightVibrantSwatch, darkVibrantSwatch).maxByOrNull {
+            if (it === vibrantSwatch) (it?.population ?: -1) * 3 else it?.population ?: -1
+        }?.rgb
     }
 }
